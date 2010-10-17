fd9828e134e7aff07ef7f5399c7d1d83b4e9ccf9
Robert Thurnher <r.thurnher@gmail.com>
2010-10-17 03:57:25 +0200

    Add some first unit testing.

--- test/all.js
+++ test/all.js
@@ -1,10 +1,29 @@
-include('ringo/unittest');
-var changeMe = require('namespace/skeleton');
+var assert = require('assert');
+var {Build} = require('ringo/cijoe/build');
 
-exports.testSomething = function () {
-    assertTrue(true);
+const SHA = 'a1a1420ad0a42f2a348c',
+      USER = 'ringo',
+      PROJECT = 'ringojs';
+
+exports['test `build` and corresponding `commit` objects'] = function () {
+    var build = new Build({
+        sha: SHA,
+        user: USER,
+        project: PROJECT,
+        projectPath: require('ringo/engine').getRingoHome().path,
+        pid: parseInt(Math.random() * 1000)
+    });
+    var commit = build.commit();
+
+    assert.isNotNull(build);
+    assert.isNotNull(commit);
+    assert.strictEqual(
+        'http://github.com/' + USER + '/' + PROJECT + '/commit/' + SHA,
+        commit.url()
+    );
+    assert.strictEqual(SHA.substring(0, 7), build.shortSha());
 };
 
 if (require.main == module.id) {
-    require('ringo/unittest').run(exports);
+    require('test').run(exports);
 }
