46291c0328ad6fe0315435c4e9c670d878a68d1a
Robert Thurnher <r.thurnher@gmail.com>
2010-10-18 00:05:12 +0200

    More testing: serializing works as expected as well.

--- test/all.js
+++ test/all.js
@@ -5,16 +5,16 @@ const SHA = 'a1a1420ad0a42f2a348c',
       USER = 'ringo',
       PROJECT = 'ringojs';
 
-exports['test `build` and corresponding `commit` objects'] = function () {
-    var build = new Build({
-        sha: SHA,
-        user: USER,
-        project: PROJECT,
-        projectPath: require('ringo/engine').getRingoHome().path,
-        pid: parseInt(Math.random() * 1000)
-    });
-    var commit = build.commit();
+var build = new Build({
+    sha: SHA,
+    user: USER,
+    project: PROJECT,
+    projectPath: require('ringo/engine').getRingoHome().path,
+    pid: parseInt(Math.random() * 1000)
+});
+var commit = build.commit();
 
+function doAssert() {
     assert.isNotNull(build);
     assert.isNotNull(commit);
     assert.strictEqual(
@@ -25,10 +25,26 @@ exports['test `build` and corresponding `commit` objects'] = function () {
     assert.matches(commit.message(), /Rhino/);
     assert.matches(commit.committedAt(), /2010/);
     assert.strictEqual(SHA.substring(0, 7), build.shortSha());
-    assert.isTrue(build.startedAt instanceof Date);
+    assert.isTrue(typeof build.startedAt === 'number');
     assert.isUndefined(build.finishedAt);
     assert.isTrue(build.building());
     assert.strictEqual('building', build.getStatus());
+}
+
+exports['test `build` and corresponding `commit` objects'] = function () {
+    doAssert();
+};
+
+exports['test serializing `build/commit` objects'] = function () {
+    var dumpPath = module.resolve('cijoe-dump-test.json'),
+        projectPath = build.projectPath;
+
+    build.dump(dumpPath);
+    build = Build.load(dumpPath, projectPath);
+    commit = build.commit();
+
+    doAssert();
+    require('fs').remove(dumpPath);
 };
 
 if (require.main == module.id) {
