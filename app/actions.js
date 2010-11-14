45491db309e3d16a77e41c20d110231b6d22a388
Robert Thurnher <r.thurnher@gmail.com>
2010-11-14 02:24:25 +0100

    Initial layout of CLI + webapp.

--- app/actions.js
+++ app/actions.js
@@ -1,7 +1,21 @@
 var {Response} = require('ringo/webapp/response');
+var {CiJoe} = require('ringo/cijoe');
+var arrays = require('ringo/utils/arrays');
 
-exports.index = function (req) {
-    return Response.skin(module.resolve('skins/index.html'), {
-        title: "It's working!"
-    });
+var joe = new CiJoe(arrays.peek(system.args));
+
+exports.ping = {
+    GET: function (req) {
+        if (joe.isBuilding() || !joe.lastBuild || !joe.lastBuild.worked()) {
+            return {
+                status: 412,
+                headers: {'Content-Type': 'text/plain'},
+                body: [
+                    joe.isBuilding() || joe.lastBuild === null ?
+                            'building' : joe.lastBuild.sha
+                ]
+            }
+        }
+        return new Response(joe.lastBuild.sha);
+    }
 };
